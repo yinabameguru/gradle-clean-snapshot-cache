63463fa34937f89b6732f985953466ee427d61e2
GitHub Action <action@github.com>
2021-07-23 14:26:25 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/yinabameguru/gradlecleansnapshotcache/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.yinabameguru.gradlecleansnapshotcache.services
+
+import com.github.yinabameguru.gradlecleansnapshotcache.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/yinabameguru/gradlecleansnapshotcache/services/MyProjectService.kt
+++ src/main/kotlin/com/github/yinabameguru/gradlecleansnapshotcache/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.yinabameguru.gradlecleansnapshotcache.services
 
+import com.github.yinabameguru.gradlecleansnapshotcache.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
